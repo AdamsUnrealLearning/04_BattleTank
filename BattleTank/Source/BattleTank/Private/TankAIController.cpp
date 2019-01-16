a30e0cc59070cd6b8ea384b9d533037b67cfd8cf
Adam Zrcek <adamzrc@gmail.com>
2019-01-16 21:37:01 +0100

    AI can find the player Controlled tank

---
+++ BattleTank/Source/BattleTank/Private/TankAIController.cpp
@@ -0,0 +1,46 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "TankAIController.h"
+
+
+
+ATank* ATankAIController::GetAITank() const
+{
+	return Cast<ATank>(GetPawn());
+}
+
+void ATankAIController::BeginPlay()
+{
+	Super::BeginPlay();
+
+	auto tank = GetAITank();
+
+	auto player = GetPlayerTank();
+
+	if (!tank) {
+		UE_LOG(LogTemp, Warning, TEXT("Not possesing a tank"))
+	}
+	else {
+		UE_LOG(LogTemp, Warning, TEXT("Tank %s calling for duty!"), *tank->GetName())
+	}
+
+	if (!player) {
+		UE_LOG(LogTemp, Warning, TEXT("\nPlayer not found"))
+	}
+	else {
+		UE_LOG(LogTemp, Warning, TEXT("\nPlayers tank %s!"), *player->GetName())
+	}
+
+}
+
+ATank * ATankAIController::GetPlayerTank() const
+{
+	auto playerPawn = GetWorld()->GetFirstPlayerController()->GetPawn();
+
+	if (!playerPawn) {
+		return nullptr;
+	}
+	else {
+		return Cast<ATank>(playerPawn);
+	}
+}

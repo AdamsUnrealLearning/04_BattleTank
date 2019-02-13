a8a138dc4725d5b03b2a606c9db464e5c5ca118f
Adam Zrcek <adamzrc@gmail.com>
2019-02-14 00:29:39 +0100

    Added TankBarrel Component and set up its Max and min elevation in degree

--- BattleTank/Source/BattleTank/Public/TankAimingComponent.h
+++ BattleTank/Source/BattleTank/Public/TankAimingComponent.h
@@ -8,7 +8,10 @@
 #include "Components/ActorComponent.h"
 #include "TankAimingComponent.generated.h"
 
+//Forward Declaration
+class UTankBarrel;
 
+// Holds barells property and Elevate method
 UCLASS( ClassGroup=(Custom), meta=(BlueprintSpawnableComponent) )
 class BATTLETANK_API UTankAimingComponent : public UActorComponent
 {
@@ -18,7 +21,7 @@ public:
 	// Sets default values for this component's properties
 	UTankAimingComponent();
 
-	void SetBarrelReference(UStaticMeshComponent* BarrelToSet);
+	void SetBarrelReference(UTankBarrel* BarrelToSet);
 
 	// TODO add turrer reference
 
@@ -27,7 +30,7 @@ public:
 
 
 private:
-	UStaticMeshComponent* Barrel = nullptr;
+	UTankBarrel* Barrel = nullptr;
 
 	void MoveBarrelTowards(FVector AimDirection);
 };
